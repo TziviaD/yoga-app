79eea25d04d74cf9d08b5efdb96a69d481b1b0d4
Thesylvia <tzivdruin@gmail.com>
2020-09-09 19:56:35 +0300

    added context processors

--- yogaplatform/context_processors.py
+++ yogaplatform/context_processors.py
@@ -1,8 +1,14 @@
-from yoga.models import Studio
+from yoga.models import *
 
 def studio(request):
+
     studio_list = []
+    if request.user.is_authenticated:
+        owned_studios = request.user.profile.owned_studios.all()
+        invited_studios = request.user.profile.studio_invites.all()
+        studio_list= owned_studios.union(invited_studios)
+    print(studio_list)
     return {
-        'all_studios': Studio.objects.all(),
+        'all_studios': studio_list,
     }
 
--- yogaplatform/settings.py
+++ yogaplatform/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'yogaplatform.custom_context_processor.subject_renderer'
+                'yogaplatform.context_processors.studio'
             ],
         },
     },
